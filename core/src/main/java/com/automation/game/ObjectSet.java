d9d164e4634e02e9423808ea36f6d3ad83783dee
Caleb McMullen <cmcmullen@gmail.com>
2026-06-22 17:17:02 -0400

    Finished get

--- core/src/main/java/com/automation/game/ObjectSet.java
+++ core/src/main/java/com/automation/game/ObjectSet.java
@@ -1,5 +1,6 @@
 package com.automation.game;
 
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 /**
@@ -15,7 +16,35 @@ public class ObjectSet<T extends WorldObject> extends TreeSet<T> {
      * @return Returns the object if it is found or null is it does not exist
      */
     public T get(int x, int y) {
-        // TODO
-        return null;
+        // If the set is empty, return immediately
+        if (this.isEmpty()) { return null; }
+
+        return getHelper(this, x, y);
+    }
+
+    /**
+     * Recursive helper method for get
+     * @param set
+     * @param x
+     * @param y
+     * @return
+     */
+    private T getHelper(SortedSet<T> set, int x, int y) {
+        // Get the first of the set and compare it to the x and y
+        T first = set.getFirst();
+        int comparison = first.compareTo(x, y);
+
+        // Base case
+        // If first has matching x and y, return it
+        if (comparison == 0) { return first; }
+
+        // Recursive case
+        // If first > x, recurse down the head side of the tree
+        if (comparison > 0) {
+            return getHelper(this.headSet(first), x, y);
+        }
+        // Otherwise recurse down the tail side
+        return getHelper(this.tailSet(first), x, y);
+
     }
 }
