0f7da402fb94a0a757bfaead9bbbf14c27a5b82b
Caleb McMullen <cmcmullen@gmail.com>
2026-06-22 10:43:55 -0400

    Changed StoneTile to not use a random texture for now. It was causing problems for negative world coordinates

--- core/src/main/java/com/automation/game/StoneTile.java
+++ core/src/main/java/com/automation/game/StoneTile.java
@@ -14,7 +14,8 @@ public class StoneTile extends Tile {
         super(manager, xPos, yPos);
 
         // Choose one of the tiles from the texture and set the texture region to point to it
-        int[] texCorner = TEX_CORNERS[xPos*yPos % 4];
+        // TODO Make this actually select one of the different textures
+        int[] texCorner = TEX_CORNERS[0];
         texRegion = new TextureRegion(texture, texCorner[0], texCorner[1], TEX_WIDTH, TEX_Height);
     }
 }
