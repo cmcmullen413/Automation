5968b6d79c6784daf9d3c134e9a834eace90a8b0
Caleb McMullen <cmcmullen@gmail.com>
2026-06-22 10:58:44 -0400

    Created a new WorldObject class that is extended by Building and Tile to contain generic things they both contain like draw and compareTo

--- core/src/main/java/com/automation/game/Building.java
+++ core/src/main/java/com/automation/game/Building.java
@@ -6,28 +6,23 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 /**
  * Abstract class for representing any building
  */
-public abstract class Building implements Comparable<Building> {
+public abstract class Building extends WorldObject {
     // Holds the path the assets folder for the building
     protected final String spritePath;
 
-    TextureRegion currentTexture;
+    protected TextureRegion currentTexture;
     int buildingSizeX;
     int buildingSizeY;
     int buildingOffsetX;
     int buildingOffsetY;
 
-    int x;
-    int y;
-
     public Building(int x, int y, int buildingSizeX, int buildingSizeY) {
+        super(x, y);
         spritePath = "assets/buildings/" + this.getClass().getSimpleName() + "/";
         this.buildingSizeX = buildingSizeX;
         this.buildingSizeY = buildingSizeY;
-        buildingOffsetX = (Tile.TILE_WIDTH - buildingSizeX)/2;
-        buildingOffsetY = (Tile.TILE_HEIGHT - buildingSizeY)/2;
-
-        this.x = x;
-        this.y = y;
+        buildingOffsetX = (TILE_WIDTH - buildingSizeX)/2;
+        buildingOffsetY = (TILE_HEIGHT - buildingSizeY)/2;
     }
 
     public abstract void update();
@@ -40,19 +35,8 @@ public abstract class Building implements Comparable<Building> {
         float realX = x+ 1f*buildingOffsetX/Tile.TILE_WIDTH;
         // TODO Figure out if centering on the y is actually needed because it doesn't seem like it
         float realY = y;// + 1f*buildingOffsetY/Tile.TILE_HEIGHT;
-        float screenX = (realX - realY) * (Tile.TILE_WIDTH/2) * camZoom - camX;
-        float screenY = (realX + realY) * (Tile.TILE_HEIGHT/2) * camZoom - camY;
+        float screenX = (realX - realY) * (TILE_WIDTH/2) * camZoom - camX;
+        float screenY = (realX + realY) * (TILE_HEIGHT/2) * camZoom - camY;
         spriteBatch.draw(currentTexture, screenX, screenY, buildingSizeX*camZoom, buildingSizeY*camZoom);
-    }
-
-    @Override
-    public int compareTo(Building other) {
-        // Sort by x first, then y
-        // Higher x or y should be ordered lower so when a set of buildings is iterated through
-        // They can be drawn from back right to front left to display them properly
-        if (this.x != other.x) { return this.x < other.x ? 1 : -1; }
-        if (this.y != other.y) { return this.y < other.y ? 1 : -1; }
-        // If the x and y are the same they are in the same square
-        return 0;
     }
 }
---
+++ core/src/main/java/com/automation/game/WorldObject.java
@@ -0,0 +1,48 @@
+package com.automation.game;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Abstract class for defining an object that can be drawn into the world. This includes things like tiles, buildings, and items
+ */
+public abstract class WorldObject implements Comparable<WorldObject> {
+
+    // The default size of a tile
+    protected static final int TILE_WIDTH = 52;
+    protected static final int TILE_HEIGHT = TILE_WIDTH/2;
+
+    // The current texture used to display the object
+    protected TextureRegion currentTexture;
+
+    // The world coordinates of the object
+    protected int x;
+    protected int y;
+
+    public WorldObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Draws the object onto the given sprite batch
+     * @param spriteBatch
+     */
+    public abstract void draw(SpriteBatch spriteBatch, float camX, float camY, float camZoom);
+
+    /**
+     * Defines the natural ordering of WorldObjects. Objects are ordered from back to front in the world space
+     * so they display overlapped correctly when drawn.
+     * @param o the object to be compared.
+     * @return
+     */
+    public int compareTo(WorldObject o) {
+        // Sort by x first, then y
+        // Higher x or y should be ordered lower so when a set of buildings is iterated through
+        // They can be drawn from back right to front left to display them properly
+        if (this.x != o.x) { return this.x < o.x ? 1 : -1; }
+        if (this.y != o.y) { return this.y < o.y ? 1 : -1; }
+        // If the x and y are the same they are in the same square
+        return 0;
+    }
+}
